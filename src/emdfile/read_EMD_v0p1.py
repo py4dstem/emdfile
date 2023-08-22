59405f107c349e19dd34fe259d9b1941b5937787
Peter Ercius ncem-gauss jupyter <percius.lbl.gov>
2023-08-21 18:20:12 -0700

    initial working version for single and multiple emd groups

--- src/emdfile/read_EMD_v0p1.py
+++ src/emdfile/read_EMD_v0p1.py
@@ -5,22 +5,55 @@ from pathlib import Path
 import h5py
 from emdfile.classes import Array
 
-verbose = True
+verbose = False
 
-def _is_emd_group(name, obj):
-    """Checks if a HDF5 item (group or dataset) is a emd v0.1 group. This is used in h5py.visititems."""
-    if isinstance(obj, h5py.Group):
-        if 'emd_group_type' in obj.attrs and obj.attrs['emd_group_type'] == 1:
-            print(f'{name} is EMDgroup')
-            emds.append(obj)
+class emd_v0p1:
+    
+    def __init__(self, filename):
+        # necessary declarations in case something goes bad
+        self.file_hdl = None
+        self.emds = []  # list of HDF5 groups with emd_data_type type 0.1
+        # check filename type, change to pathlib.Path
+        if isinstance(filename, str):
+            filename = Path(filename)
+        elif isinstance(filename, Path):
+            pass
         else:
-            print(f'{name} is regular group')
-    elif isinstance(obj, h5py.Dataset):
-        print(f'{name} is dataset')
-    else:
-        print(f'{name} is an unknown instance')
+            raise TypeError('Filename is supposed to be a string or pathlib.Path or file object')
 
-
+        # try opening the file
+        try:
+            self.file_hdl = h5py.File(filename, 'r')
+        except:
+            print('Error opening file for readonly: "{}"'.format(filename))
+            raise
+        
+        self.file_hdl.visititems(self._is_emd_group)
+        
+    def __del__(self):
+        self.file_hdl.close()
+    
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.__del__()
+        return None
+    
+    def _is_emd_group(self, name, obj):
+        """Checks if a HDF5 item (group or dataset) is a emd v0.1 group. This is used in h5py.visititems."""
+        if isinstance(obj, h5py.Group):
+            if 'emd_group_type' in obj.attrs and obj.attrs['emd_group_type'] == 1:
+                if verbose:
+                    print(f'{name} is EMDgroup')
+                self.emds.append(obj)
+    
+    def _is_EMD_v0p1(self):
+        if self.file_hdl.attrs['version_major'] == 0 and self.file_hdl.attrs['version_minor'] == 1:
+            return True
+        else:
+            return False
+        
 def read_EMD_v0p1(
     filepath,
     ):
@@ -34,45 +67,50 @@ def read_EMD_v0p1(
         (Array) the data
     """
     
-    ans = None
+    ans = []
+    emd0 = None
     
+   
     # determine if the file is EMD 1.0
     # if not, try reading it as an EMD 0.1
-    if not _is_EMD_file(filepath):
-        read_EMD_v0p1(filepath)
+    #if not _is_EMD_file(filepath):
+    #    read_EMD_v0p1(filepath)
 
     # TODO
     # walk the filetree, look for, and load an EMD 0.1 group
-    emds = []
-    with h5py.File(filepath, r) as emd0:
-        emd0.visititems(_is_emd_group)
-        if verbose:
-            print(emds)
+    with emd_v0p1(filepath) as emd0:
 
+#         print('Available emds:', emd0.emds)
         # if no EMD 0.1 group is found, or if it is unreadable, raise an exception
-        if len(emds < 1):
+        if len(emd0.emds) < 1:
             raise Exception("No EMD 0.1 groups found!")
         
-        # TODO
+        print('avail emds', emd0.emds)
+        
         # read in the groups
-        for emd_group in emds:
-            data = emd_group['data']
+        for emd_group in emd0.emds:
+            data = emd_group['data'][:]
             
             dims = []
+            dim_units = []
+            dim_names = []
             for ii in range(data.ndim):
-                dims.append(emd_group[f'dim{ii+1}'])
-        
+                cur_dim = emd_group[f'dim{ii+1}']
+                dims.append(cur_dim[:])
+                dim_units.append(cur_dim.attrs['units'][:])
+                dim_names.append(cur_dim.attrs['name'][:])
+            
             # TODO
             # put the data into an Array
-            #ans = Array(
-            #    data = data,
-            #    name = ,
-            #    units = ,
-            #    dims = ,
-            #    dim_units = ,
-            #    dim_names = ,
-            #)
+#             ans = Array(
+#                 data = data[:],
+#                 name = emd_group.name,
+#                 units = None,
+#                 dims = dims,
+#                 dim_units = dim_units,
+#                 dim_names = dim_names,
+#             )
+            ans.append(data)
 
-    # return
     return ans
 
--- test/test_emd01.py
+++ test/test_emd01.py
@@ -15,6 +15,9 @@ import numpy as np
 from numpy import array_equal
 import h5py
 
+from emdfile.read_EMD_v0p1 import read_EMD_v0p1, emd_v0p1
+
+
 from emdfile import _TESTPATH
 from emdfile import save,read
 from emdfile.read import _is_EMD_file,_get_EMD_rootgroups
@@ -30,29 +33,33 @@ from emdfile.classes import (
 class TestEMD01:
     
         @pytest.fixture
-        def temp_file(self):
+        def _temp_file(self):
             """Create an empty temporary file and return as a Path."""
             tt = tempfile.NamedTemporaryFile(mode='wb')
             tt.close()  # need to close the file to use it later
             return Path(tt.name)
         
         @pytest.fixture
-        def _make_data(self, temp_file):
+        def _make_data(self):
             """Create a v0.1 (or 0.2?) version EMD using a temporary file. Returns a Path
             to the file.
             """
+            """Create an empty temporary file and return as a Path."""
+            tt = tempfile.NamedTemporaryFile(mode='wb')
+            tt.close()  # need to close the file to use it later
+            _temp_file = Path(tt.name)
+            
+            data = np.random.rand(100, 100)
 
-            data = np.random.rand(100,100)
-
-            with h5py.File(temp_file, 'w') as f0:
+            with h5py.File(_temp_file, 'w') as f0:
                 f0.attrs['version_major'] = 0
                 f0.attrs['version_minor'] = 1
-                f0.create_group('/data')
+                data_top = f0.create_group('/data')
                 f0.create_group('/microscope')
                 f0.create_group('/user')
                 f0.create_group('/sample')
 
-                group_top = f0.create_group('/data/test_data')
+                group_top = data_top.create_group('test_data')
                 group_top.attrs['emd_group_type'] = int(1)
                 group_top.create_dataset('data', data=data)
                 dim1 = group_top.create_dataset('dim1', data=range(100))
@@ -61,7 +68,37 @@ class TestEMD01:
                 dim2 = group_top.create_dataset('dim2', data=range(100))
                 dim2.attrs['name'] = 'X'
                 dim2.attrs['units'] = 'pixels'
-            return temp_file
+            return _temp_file
+        
+        @pytest.fixture
+        def _make_data_2(self):
+            """Create a v0.1 (or 0.2?) version EMD using a temporary file. Returns a Path
+            to the file.
+            """
+            tt = tempfile.NamedTemporaryFile(mode='wb')
+            tt.close()  # need to close the file to use it later
+            _temp_file = Path(tt.name)
+            
+            with h5py.File(_temp_file, 'w') as f0:
+                f0.attrs['version_major'] = 0
+                f0.attrs['version_minor'] = 2
+                data_top = f0.create_group('/data')
+                f0.create_group('/microscope')
+                f0.create_group('/user')
+                f0.create_group('/sample')
+                for ii in range(2):
+                    data = np.random.rand(100, 100)
+                    group_name = f'test_data{ii}'
+                    group_top = data_top.create_group(group_name)
+                    group_top.attrs['emd_group_type'] = int(1)
+                    group_top.create_dataset('data', data=data)
+                    dim1 = group_top.create_dataset('dim1', data=range(100))
+                    dim1.attrs['name'] = 'Y'
+                    dim1.attrs['units'] = 'pixels'
+                    dim2 = group_top.create_dataset('dim2', data=range(100))
+                    dim2.attrs['name'] = 'X'
+                    dim2.attrs['units'] = 'pixels'
+            return _temp_file
         
         def test_make_data(self, _make_data):
             """Test that the temporary data is created with the emd v0.1 format."""
@@ -73,3 +110,10 @@ class TestEMD01:
         def test_not_emd1(self, _make_data):
             """Ensure it is not seen as a emd v1.0"""
             assert not _is_EMD_file(_make_data)
+            
+        def test_read(self, _make_data):
+            new_emd = read_EMD_v0p1(_make_data)
+
+        def test_read2(self, _make_data_2):
+            new_emd = read_EMD_v0p1(_make_data_2)
+
