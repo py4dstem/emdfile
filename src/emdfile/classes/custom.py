0ac87079e44135271b17a3d3081eee46b9e8221f
bsavitzky <ben.savitzky@gmail.com>
2024-09-04 22:01:45 -0400

    updates

--- src/emdfile/classes/custom.py
+++ src/emdfile/classes/custom.py
@@ -5,6 +5,51 @@ from emdfile.classes.utils import _get_class
 
 class Custom(Node):
     """
+    The purpose of Custom nodes is to support creation of new classes by
+    composition of emdfile classes. To do so, create a class inheriting
+    from Custom, then add attributes which are emdfile classes
+
+        >>> class X(Custom):
+        >>>     def __init__(self, name):
+        >>>         Custom.__init__(self, name=name)
+        >>>         self.a = Array(np.ones((2,2)),'a')
+
+    For some X instance x, on write x.a is saved, as is any attribute
+    pointing to a Node instance. Including
+
+        >>>         ...
+        >>>         self.b = Custom('b')
+        >>>         b.c = Array((3,3),'c')
+
+    results in x.b and x.c saving on write as well, nesting the b.c
+    group in the HDF5. Storing metadata is possible in the usual way
+    (see the Node doctring) in x, x.a, a.b, and x.c.
+
+    Downstream Integration
+    ----------------------
+    See the Node docstring for introductory remarks. A class inheriting from
+    Custom must define ._get_constructor_args and ._populate_instance to read
+    attribute nodes correctly.
+
+        >>> @classmethod
+        >>> def _get_constructor_args(cls, group):
+        >>>     md = Node.from_h5(group).metadata
+        >>>     d = cls._get_emd_attr_data(group)
+        >>>     ars = {
+        >>>         'x' : md['x'],
+        >>>         'y' : d['y'],
+        >>>         'z' : d['y'].metadata['z']
+        >>>     }
+        >>> return args
+
+    fetches metadata and data and returns an argument dictionary, and
+
+        >>> def _populate_instance(self, group):
+        >>>     d = cls._get_emd_attr_data(group)
+        >>>     self.a = d['a']
+
+    populates a node-like attribute after instantiation. ._get_emd_attr_data
+    returns a dictionary of all node-like attributes.
     """
     _emd_group_type = 'custom'
     def __init__(self,name='custom'):
@@ -20,16 +65,29 @@ class Custom(Node):
 
         Classes inheriting from Custom are required to overwrite this method.
         See the Node class docstring for additional information. To retrieve
-        all custom sub-nodes, use ._get_emd_attr_data. If metadata items
-        are needed for instantiation, a Node can be created containing the
-        metadata only using
+        all custom sub-nodes, use ._get_emd_attr_data. To retrieve to top
+        level node metadata, use
 
-            >>> _node = Node.from_h5(group)
+            >>> md = Node.from_h5(group).metadata
         """
         raise Exception(f"Custom class {cls} needs a `_get_constructor_args` method!")
 
+    def _populate_instance(self,group):
+        """
+        Run while reading an object from file after instantiation.
+
+        Classes inheriting from Custom are required to overwrite this method.
+        See the Node class docstring for additional information. To retrieve
+        all custom sub-nodes, use ._get_emd_attr_data. To retrieve to top
+        level node metadata, use
+
+            >>> md = Node.from_h5(group).metadata
+        """
+        raise Exception(f"Custom class {cls} needs a `_populate_instance` method!")
+
     def _get_emd_attr_data(self,group):
-        """ Loops through h5 groups under group, finding and returning
+        """
+        Loops through h5 groups under group, finding and returning
         a {name:instance} dictionary of {attribute name : class instance}
         pairs from all 'custom_' subgroups
         """
